6e18b3d97c8876222a92d900b133f738847657df
wattanx <wattan.dev@gmail.com>
2024-09-12 10:10:34 +0900

    chore: add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Nuxt Server Components Example',
+    },
+  },
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/global.css'],
