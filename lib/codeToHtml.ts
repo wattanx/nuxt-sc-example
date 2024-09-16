8df98e9a0b848a404a38b5ff0e55672f9f3e1df7
wattanx <wattan.dev@gmail.com>
2024-09-16 16:48:23 +0900

    fix: add engine

--- lib/codeToHtml.ts
+++ lib/codeToHtml.ts
@@ -1,10 +1,11 @@
-import { createHighlighterCore } from 'shiki/core';
+import { createHighlighterCore, createWasmOnigEngine } from 'shiki/core';
 
 export async function codeToHtml(code: string, lang: 'javascript' | 'vue') {
   const highlighter = await createHighlighterCore({
     themes: [import('shiki/themes/vitesse-dark.mjs')],
     langs: [import('shiki/langs/javascript.mjs'), import('shiki/langs/vue.mjs')],
     loadWasm: import('shiki/wasm'),
+    engine: createWasmOnigEngine(import('shiki/wasm')),
   });
 
   return highlighter.codeToHtml(code, {
