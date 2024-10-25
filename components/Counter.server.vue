4c160a8c7a581e50b88dd53897ae216892e0c795
wattanx <wattan.dev@gmail.com>
2024-10-25 16:13:35 +0900

    fix: fix client interactivity example

---
+++ components/Counter.server.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0);
+</script>
+
+<template>
+  <div
+    class="relative rounded-md border border-dashed border-gray-400 bg-blue-900 p-6"
+  >
+    <div class="space-y-4">
+      <p class="absolute -top-4 rounded-full bg-gray-600 px-4 py-2 text-sm">
+        Server Component
+      </p>
+      <p>This is not interactive.</p>
+      <p>Count: {{ count }}</p>
+
+      <button class="mt-2 rounded-md bg-gray-800 px-2 py-1" @click="count++">
+        increment
+      </button>
+    </div>
+
+    <slot />
+  </div>
+</template>
--- pages/examples/client-interactivity.vue
+++ pages/examples/client-interactivity.vue
@@ -8,16 +8,15 @@ const count = ref(0);
 
 <template>
   <Boundary label="Client Component">
-    <button class="mt-2 rounded-md bg-gray-800 px-2 py-1" @click="count++">
-      increment
-    </button>
-    <CodeExample :count class="mt-6">
+    <Counter class="mt-6">
       <Boundary label="Client Component" class="mt-6">
-        <p>Client Components can be put in the Server Components slot.</p>
-        <button class="mt-2 rounded-md bg-gray-800 px-2 py-1" @click="count--">
-          decrement
+        <p>This is interactive.</p>
+
+        <p>Count: {{ count }}</p>
+        <button class="mt-2 rounded-md bg-gray-800 px-2 py-1" @click="count++">
+          increment
         </button>
       </Boundary>
-    </CodeExample>
+    </Counter>
   </Boundary>
 </template>
